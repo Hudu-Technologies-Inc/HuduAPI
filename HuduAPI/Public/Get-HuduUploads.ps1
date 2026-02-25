4987eee48da4f14f663fd729531d718d261d42a3
masonville17 <anoniboni9876@gmail.com>
2026-02-25 16:42:19 -0500

    version-dependent pagination shouldbe good

--- HuduAPI/Public/Get-HuduUploads.ps1
+++ HuduAPI/Public/Get-HuduUploads.ps1
@@ -18,7 +18,13 @@ function Get-HuduUploads {
     if ($Id) {
         $Upload = Invoke-HuduRequest -Method Get -Resource "/api/v1/uploads/$Id"
     } else {
-        $Upload = Invoke-HuduRequest -Method Get -Resource "/api/v1/uploads"
+        [version]$script:Version = $script:Version ?? (Get-HuduAppInfo).version
+        if ($script:Version -ge [version]("2.41.0")) {
+            $Upload = Invoke-HuduRequestPaginated -hudurequest @{Method = "Get"; Resource = "/api/v1/uploads"; property = "uploads"}
+        } else {
+            $Upload = Invoke-HuduRequest -Method Get -Resource "/api/v1/uploads"
+        }
     }
     return $Upload
 }
+ 
