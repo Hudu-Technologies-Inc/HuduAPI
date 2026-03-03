3097c26319b6580e0c87e85d62f27cbb96eb56ed
masonville17 <anoniboni9876@gmail.com>
2026-03-03 14:18:10 -0500

    improved logic for allowed typces

--- HuduAPI/Private/Assert-AllowedObjectType.ps1
+++ HuduAPI/Private/Assert-AllowedObjectType.ps1
@@ -4,9 +4,11 @@ function Assert-AllowedObjectType {
         [Parameter(Mandatory)][string[]]$AllowedCanonicals
     )
 
-    $canonical = Get-ObjectTypeFromCononical $InputType
+    $canonical = Get-ObjectTypeFromCononical $InputType  # accepts aliases; throws if unknown
+
     if ($canonical -notin $AllowedCanonicals) {
         throw "Invalid type '$InputType' (canonical: '$canonical'). Allowed: $($AllowedCanonicals -join ', ')"
     }
+
     $true
 }
