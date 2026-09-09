76e31298eff23eb8583b3c9b21c5f20e96ad015d
masonville17 <anoniboni9876@gmail.com>
2026-09-09 16:08:17 -0400

    revert change on updatedoc

--- Tools/Update-Documentation.ps1
+++ Tools/Update-Documentation.ps1
@@ -2,5 +2,7 @@
 # Remove-Module HuduAPI
 # Import-Module .\Output\HuduAPI\HuduAPI.psd1
 # New-MarkdownHelp -Module HuduAPI -OutputFolder .\Docs\ -Force
-import-module C:\Users\Administrator\Documents\GitHub\hapidev\HuduAPI\HuduAPI.psd1
+
+remove-module huduapi -Force
+
 New-MarkdownHelp -Module HuduAPI -OutputFolder .\Docs\ -Force
