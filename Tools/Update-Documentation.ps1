828d4b7a11421e36877af18fb280e4d4bbf65fdc
masonville17 <anoniboni9876@gmail.com>
2026-09-09 16:07:03 -0400

    generated MD for pin/unpin cmdlets

--- Tools/Update-Documentation.ps1
+++ Tools/Update-Documentation.ps1
@@ -2,7 +2,5 @@
 # Remove-Module HuduAPI
 # Import-Module .\Output\HuduAPI\HuduAPI.psd1
 # New-MarkdownHelp -Module HuduAPI -OutputFolder .\Docs\ -Force
-
-remove-module huduapi -Force
-
+import-module C:\Users\Administrator\Documents\GitHub\hapidev\HuduAPI\HuduAPI.psd1
 New-MarkdownHelp -Module HuduAPI -OutputFolder .\Docs\ -Force
